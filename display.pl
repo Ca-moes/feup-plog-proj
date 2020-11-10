c087719634e43db76b20cdca1c45fd2caa760daf
Ca-moes <andre3000pt@gmail.com>
2020-11-10 11:37:54 +0000

    display flexivel com tamanho do board

--- display.pl
+++ display.pl
@@ -1,5 +1,5 @@
 % Initial board with pieces in chess formation
-/* initial_board([
+initial_board([
   [1,2,1,2,1,2,1,2],
   [2,1,2,1,2,1,2,1],
   [1,2,1,2,1,2,1,2],
@@ -8,9 +8,9 @@
   [2,1,2,1,2,1,2,1],
   [1,2,1,2,1,2,1,2],
   [2,1,2,1,2,1,2,1]
-]). */
+]).
 
-initial_board([
+/* initial_board([
   [1,2,1,2,1,2],
   [2,1,2,1,2,1],
   [1,2,1,2,1,2],
@@ -18,6 +18,12 @@ initial_board([
   [1,2,1,2,1,2],
   [2,1,2,1,2,1]
 ]).
+
+initial_board([
+  [1,2,1],
+  [2,1,2],
+  [1,2,1]
+]). */
 
 size_of_board(Board, X, Y):-
   nth0(0, Board, Header),
@@ -39,17 +45,23 @@ row(5, R) :- R='F'.
 row(6, R) :- R='G'.
 row(7, R) :- R='H'.
 
+print_board_middle_separator(1):-
+  write('|\n').
+print_board_middle_separator(X):-
+  write('+ - '), X1 is X-1, print_board_middle_separator(X1).
+
+
 % When the counter reaches 0, it ends
-print_matrix([], 8).
-print_matrix([L|T], N) :-
+print_matrix([], 8, X).
+print_matrix([L|T], N, X) :-
   row(N, R), code(1,P),write(' '), write(R), write('   '),put_code(P), write(' | '),
   N1 is N + 1,
   print_line(L), nl,
-  N < 7, !, write('---    | - + - + - + - + - + - + - + - |\n'),
-  print_matrix(T, N1).
-print_matrix(A, B):-
-  write('---    *---+---+---+---+---+---+---+---*\n'),
-  print_matrix([], 8).
+  N < X - 1, !, write('---    | - '), print_board_middle_separator(X),
+  print_matrix(T, N1, X).
+print_matrix(A, B, X):-
+  write('---    *---'),
+  print_board_separator(X).
 
 % Prints a line of the board
 print_line([]):-
@@ -59,10 +71,26 @@ print_line([C|L]) :-
   print_line(L).
 
 print_header(Inicial, Final):-
-  Inicial = Final.
+  Inicial = Final,
+  write('\n').
 print_header(Inicial, Final):-
   write(' '), write(Inicial), write(' |'), N1 is Inicial + 1, print_header(N1, Final).
+
+print_separator(0):-
+  write('|\n').
+print_separator(X):-
+  write('+---'), X1 is X-1, print_separator(X1).
   
+print_header_codes(0, P):-
+  write('\n').
+print_header_codes(X, P):-
+  put_code(P), write('   '), X1 is X-1, print_header_codes(X1, P).
+
+print_board_separator(1):-
+  write('*\n').
+print_board_separator(X):-
+  write('+---'), X1 is X-1, print_board_separator(X1).
+
 
 % Prints the board according to its state
 display_game(Board, Player) :-
@@ -71,12 +99,15 @@ display_game(Board, Player) :-
   size_of_board(Board, X, Y),
   write('       |'),
   print_header(0, X),
-  write('\n'),
-  write('       +---+---+---+---+---+---+---+---|\n'),
-  write('         '), put_code(P), write('   '), put_code(P),  write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P),write('\n'),
-  write('---    *---+---+---+---+---+---+---+---*\n'),
-  print_matrix(Board, 0),
-  write('         '), put_code(P), write('   '), put_code(P),  write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P), write('   '), put_code(P),write('\n').
+  write('       '),
+  print_separator(X),
+  write('         '), 
+  print_header_codes(X, P),
+  write('---    *---'),
+  print_board_separator(X),
+  print_matrix(Board, 0, X),
+  write('         '),
+  print_header_codes(X, P).
 
 % test display:
 % ?- initial_board(_B), print_board(_B).
