24b759d2210b84add0abf6eaa9a291610a8f2ee6
Ca-moes <andre3000pt@gmail.com>
2020-11-19 21:43:30 +0000

    very nice

--- bot.pl
+++ bot.pl
@@ -7,6 +7,8 @@ Jogada do Computador: Escolha da jogada a efetuar pelo computador,
 dependendo do nível de dificuldade. O predicado deve chamar-se
 choose_move(+GameState, +Player, +Level, -Move).
 
-Lista de Jogadas Válidas: Obtenção de lista com jogadas possíveis. O predicado
-deve chamar-se valid_moves(+GameState, +Player, -ListOfMoves).
+Lista de Jogadas Válidas: Obtenção de lista com jogadas possíveis (com melhor á cabeça). O predicado
+deve chamar-se 
+valid_moves(+GameState, +Player, -ListOfMoves).
+[[X,Y,'up'],[X,Y,'left']]
 */
