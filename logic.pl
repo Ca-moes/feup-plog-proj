c2353e89ddd6631661fc6a17e80547ea403c0e7d
Ca-moes <andre3000pt@gmail.com>
2020-11-19 15:39:38 +0000

    documentar tudo

--- logic.pl
+++ logic.pl
@@ -1,13 +1,12 @@
 % After the input is processed correctly, the game logic is made here
 
-% directiosn knowledge
+% directions knowledge
 direction(1, 'up').
 direction(2, 'right').
 direction(3, 'down').
 direction(4, 'left').
 
 % get position in direction, doesn't check range, can get out of board
-% Not beeing used
 direction(X, Y, 'up', Xr, Yr):-     Xr = X,     Yr is Y-1.
 direction(X, Y, 'right', Xr, Yr):-  Xr is X+1,  Yr = Y.
 direction(X, Y, 'down', Xr, Yr):-   Xr = X,     Yr is Y+1.
@@ -22,27 +21,23 @@ move(GameState, PlayerS, NewGameState) :-
     format('- Direction received in logic : ~s\n', Direction),
     make_choice(GameState, PlayerS, X, Y, Direction, NewGameState).
     
-%% Ripzão na simplificação, ele volta para tras
+% predicate to read input, check if piece belongs to player, gwt available directions and return
 choose_piece(Board, PlayerS, X, Y, Directions):-
     size_of_board(Board, Size),
     read_inputs(Size, Xread, Yread),
     validate_choice(Board, Xread, Yread, PlayerS, Xtemp, Ytemp),
     available_dirs(Board, Xtemp, Ytemp, PlayerS, List),
-    (
-        (
-            List \== [],
-            write('- There are plays available for that spot\n'),
-            X is Xtemp, Y is Ytemp, Directions = List
-        ) ;
-        (
-            write('# No plays available for that piece, choose another\n'),
-            skip_line,
-            choose_piece(Board, PlayerS, X1, Y1, Directions1),
-            X is X1, Y is Y1, Directions = Directions1
-        )
-    ).
+    List \== [],
+    write('- There are plays available for that spot\n'),
+    X is Xtemp, Y is Ytemp, Directions = List.
+% in case there are no available directions, choose_piece again
+% doesn't enter here, instead goes back to validate_choice when there aro no directions
+choose_piece(Board, PlayerS, X, Y, Directions):-
+    write('# No plays available for that piece, choose another\n'),
+    skip_line,
+    choose_piece(Board, PlayerS, X, Y, Directions).
 
-
+     
 % check if selected piece belongs to player
 validate_choice(Board, Xread, Yread, PlayerS, X, Y):-
     row(NumbY, Yread),
@@ -51,6 +46,7 @@ validate_choice(Board, Xread, Yread, PlayerS, X, Y):-
     Piece == Value,
     X = Xread, Y = NumbY,
     write('- Chose Spot belonging to player\n').
+% if the selected piece doesnt belong to the player, asks again
 validate_choice(Board, _, _, PlayerS, X, Y):-
     write('--Unavailable piece, try again\n'),
     size_of_board(Board, Size),
@@ -70,39 +66,48 @@ player_in_board(Board, X, Y, PlayerS):-
         
 % returns in Lists the numbers of the directions available to go
 available_dirs(Board, X, Y, PlayerS, List):-
-    %vai ter de verificar todas as direções
+    %needs to check all 4 directions
     check_dir(Board, X, Y, PlayerS, 'up', ListItem1), append([], ListItem1, PreList1),
     check_dir(Board, X, Y, PlayerS, 'right', ListItem2), append(PreList1, ListItem2, PreList2),
     check_dir(Board, X, Y, PlayerS, 'down', ListItem3), append(PreList2, ListItem3, PreList3),
-    check_dir(Board, X, Y, PlayerS, 'left', ListItem4), append(PreList3, ListItem4, List).
-    
+    check_dir(Board, X, Y, PlayerS, 'left', ListItem4), append(PreList3, ListItem4, List), !.
 
-% Checks if there's a enemy piece Direction of position
-check_dir(Board, X, Y, PlayerS, Direction, Result):-
+% Checks if there is an enemy piece on the 'up' direction
+check_dir(Board, X, Y, PlayerS, 'up', ['up']):-
+    opposed_opponent_code(PlayerS, ExpectedCode),
+    Y > 0, Y1 is Y-1, value_in_board(Board, X, Y1, Value), Value==ExpectedCode.
+% Checks if there is an enemy piece on the 'right' direction
+check_dir(Board, X, Y, PlayerS, 'right', ['right']):-
     size_of_board(Board, Size),
     opposed_opponent_code(PlayerS, ExpectedCode),
-    % OU para aceder a uma direção
-    ((direction(1, Direction) , Y > 0, Y1 is Y-1, value_in_board(Board, X, Y1, Value), Value==ExpectedCode, Result = [Direction]);
-    (direction(2, Direction) , X < Size, X1 is X+1, value_in_board(Board, X1, Y, Value), Value==ExpectedCode, Result = [Direction]);
-    (direction(3, Direction) , Y < Size, Y2 is Y+1, value_in_board(Board, X, Y2, Value), Value==ExpectedCode, Result = [Direction]);
-    (direction(4, Direction) , X > 0, X2 is X-1, value_in_board(Board, X2, Y, Value), Value==ExpectedCode, Result = [Direction]);
-    Result = []).
+    X < Size, X1 is X+1, value_in_board(Board, X1, Y, Value), Value==ExpectedCode.
+% Checks if there is an enemy piece on the 'down' direction
+check_dir(Board, X, Y, PlayerS, 'down', ['down']):-
+    size_of_board(Board, Size),
+    opposed_opponent_code(PlayerS, ExpectedCode),
+    Y < Size, Y2 is Y+1, value_in_board(Board, X, Y2, Value), Value==ExpectedCode.
+% Checks if there is an enemy piece on the 'left' direction
+check_dir(Board, X, Y, PlayerS, 'left', ['left']):-
+    opposed_opponent_code(PlayerS, ExpectedCode),
+    X > 0, X2 is X-1, value_in_board(Board, X2, Y, Value), Value==ExpectedCode.
+% returns a empty list in case of a wrong direction
+check_dir(_, _, _, _, _, []).
 
-
+% replaces Element E in List L at Index I, Resulting in List K
 replace_index(I, L, E, K) :-
     nth0(I, L, _, R),
     nth0(I, K, E, R).
 
+% replaces a value in the board
 replace(Board, X, Y, Value, BoardResult):-
     %usar substitute(+X, +Xlist, +Y, ?Ylist)
     nth0(Y, Board, Row),
     replace_index(X, Row, Value, NewRow),
     replace_index(Y, Board, NewRow, BoardResult).
-    
+
+%  performs the change in the board, replaces current piece with 0 and enemy piece with player code
 make_choice(Board, PlayerS, X, Y, Direction, FinalBoard):-
-    % X Y fica a branco
     replace(Board, X, Y, 0, Board1),
-    % Direção fica com peça de player
     direction(X, Y, Direction, X1, Y1),
     player_piece(PlayerS, Code),
     replace(Board1, X1, Y1, Code, FinalBoard).
@@ -123,6 +128,7 @@ check_final_state(GameState, PlayerS, X, Y):-
     next_index(X, Y, Length, X2, Y2),
     check_final_state(GameState, PlayerS, X2, Y2).
 
+% gets next index and verifies if reached end of Row, in that case switched to next row
 next_index(X, Y, Length, X2, Y2):-
     X1 is X + 1,
     X1 \== Length,
@@ -134,7 +140,7 @@ next_index(X, Y, Length, X2, Y2):-
     X2 is 0,
     Y2 is Y + 1.
 
-
+% used before calling next_index to check if current position is the last in the board
 check_end(X, Y, Length, 1):-
     X is (Length - 1),
     Y is (Length - 1).
@@ -153,7 +159,7 @@ check_no_neighbors(Board, PlayerS, X, Y, Value, 1):-
 % if the player piece is different from the value on the board, no need to check for directions
 check_no_neighbors(_, _, _, _, _, 1).
 
-
+% instead of moving a pice and captuing a enemy piece, a player piece is removed because there aren't any available plays
 remove(GameState, PlayerS, NewGameState) :-
     write('- There are no pieces to replace, select one piece to remove.\n'),
     size_of_board(GameState, Size),
--- play.pl
+++ play.pl
@@ -12,46 +12,43 @@ initial(GameState) :-
 start_game(GameState) :-
   turn(GameState, 'Player 1', Result).
 
-% Makes a turn and calls the predicate to chose a spot
+% Checks if game is in final state (remove pieces intead of relocating them) and in that case, removes piece, checks if there's a winner and processes
 turn(GameState, PlayerS, Result) :-
   format('\n ~a turn.\n', PlayerS),
-   % verificar se está em fase final, AKA tirar peças em vez de comer
   check_final_state(GameState, PlayerS, 0, 0),
   remove(GameState, PlayerS, NewGameState),
   check_winnner(NewGameState, PlayerS, TempResult), 
   process_result(TempResult, NewGameState, PlayerS, Result).  
+% Move a piece instead of removing one, checks if there's a winner and processes
 turn(GameState, PlayerS, Result) :-
   move(GameState, PlayerS, NewGameState),
   check_winnner(NewGameState, PlayerS, TempResult), 
   process_result(TempResult, NewGameState, PlayerS, Result).
 
-
+% if there's no winner the next turn is played by the enemy
 process_result('none', NewGameState, PlayerS, Result):-
   display_game(NewGameState),
   opposed_opponent_string(PlayerS, EnemyS),
   skip_line,
   turn(NewGameState, EnemyS, Result).
+% if there's a winner, the game ends
 process_result(Winner, _, _, Result):-
   format('Result -> ~s', Winner),
   Result = Winner.
 
-%% Ripzão na simplificação, ele volta para tras e tenta com Size1 diferentes
-check_winnner(Board, CurrentPlayer, Player):-
+% checks first if enemy is winner
+check_winnner(Board, CurrentPlayer, EnemyS):-
   size_of_board(Board, Size),
   Size1 is Size-1,
   opposed_opponent_string(CurrentPlayer, EnemyS),
-  check_win(EnemyS, Board, Size1, Result), 
-  (
-    (Result == EnemyS, Player=EnemyS)
-    ;
-    (
-      check_win(CurrentPlayer, Board, Size1, Result1), 
-      (
-        (Result1 == CurrentPlayer, Player=CurrentPlayer);
-        (Player = 'none')
-      )
-    )
-  ).
+  check_win(EnemyS, Board, Size1, EnemyS).
+% then checks if player is the winner
+check_winnner(Board, CurrentPlayer, CurrentPlayer):-
+  size_of_board(Board, Size),
+  Size1 is Size-1,
+  check_win(CurrentPlayer, Board, Size1, CurrentPlayer).
+% in case there is no winner
+check_winnner(_, _, 'none').
 
 % verifies if player 1 won, and if the helper function finds a solution, returns 'Player 1'
 check_win('Player 1', Board, Y, 'Player 1'):-
@@ -59,14 +56,14 @@ check_win('Player 1', Board, Y, 'Player 1'):-
   attemp_flood_fill(Board, 0, Y, NewBoard),
   size_of_board(Board, Size),
   Size1 is Size-1,
-  check_1_win_helper(NewBoard, Size1, Size1, found).
+  check_1_win_helper(NewBoard, Size1, Size1).
 % if the helper functions doesn't find a solution, goes to next predicate
 check_win('Player 1', Board, Y, _):-
   value_in_board(Board, 0, Y, 0), 
   attemp_flood_fill(Board, 0, Y, NewBoard),
   size_of_board(Board, Size),
   Size1 is Size-1,
-  check_1_win_helper(NewBoard, Size1, Size1, 'not'), fail.
+  check_1_win_helper(NewBoard, Size1, Size1).
 % if the value in board is not 0, goes to next spot
 check_win('Player 1', Board, Y, Result):-
   Y1 is Y-1, Y1 >= 0, 
@@ -80,14 +77,14 @@ check_win('Player 2', Board, X, 'Player 2'):-
   size_of_board(Board, Size),
   floodFill(Board, Size, X, 0, 0, 9, NewBoard),
   Size1 is Size-1,
-  check_2_win_helper(NewBoard, Size1, Size1, 'found').
+  check_2_win_helper(NewBoard, Size1, Size1).
 % if the helper functions doesn't find a solution, goes to next predicate
 check_win('Player 2', Board, X, 'Player 2'):-
   value_in_board(Board, X, 0, 0), 
   size_of_board(Board, Size),
   floodFill(Board, Size, X, 0, 0, 9, NewBoard),
   Size1 is Size-1,
-  check_2_win_helper(NewBoard, Size1, Size1, 'not'), fail.
+  check_2_win_helper(NewBoard, Size1, Size1).
 % if the value in board is not 0, goes to next spot
 check_win('Player 2', Board, X, Result):-
   X1 is X-1, X1 >= 0, 
@@ -96,26 +93,22 @@ check_win('Player 2', Board, X, Result):-
 check_win('Player 2', _, _, 'none').
 
 % if the value in board in 9, a solution was found
-check_1_win_helper(Board, Y, MaxX, 'found'):-
+check_1_win_helper(Board, Y, MaxX):-
   value_in_board(Board, MaxX, Y, 9).
 % else goes to the next spot and checks
-check_1_win_helper(Board, Y, MaxX, Return):-
+check_1_win_helper(Board, Y, MaxX):-
   Y1 is Y-1,
   Y1 >= 0,
-  check_1_win_helper(Board, Y1, MaxX, Return).
-% once it reaches the end of the board, returns 'not'
-check_1_win_helper(_, _, _, 'not').
+  check_1_win_helper(Board, Y1, MaxX).
 
 % if the value in board in 9, a solution was found
-check_2_win_helper(Board, X, MaxY, 'found'):-
+check_2_win_helper(Board, X, MaxY):-
   value_in_board(Board, X, MaxY, 9).
 % else goes to the next spot and checks
-check_2_win_helper(Board, X, MaxY, Return):-
+check_2_win_helper(Board, X, MaxY):-
   X1 is X-1,
   X1 >= 0,
-  check_2_win_helper(Board, X1, MaxY, Return).
-% once it reaches the end of the board, returns 'not'
-check_2_win_helper(_, _, _, 'not').
+  check_2_win_helper(Board, X1, MaxY).
 
 % does one floodfill and doesn't repeat on redo
 attemp_flood_fill(Board, X, Y, NewBoard):-
